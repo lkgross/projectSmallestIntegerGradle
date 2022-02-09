d11b57a97337ff42ecb7e514b92392bd16969095
L. K. Gross <42473922+lkgross@users.noreply.github.com>
2022-02-09 11:05:28 -0500

    Update SmallestInteger.java

--- src/main/java/bsu/comp152/SmallestInteger.java
+++ src/main/java/bsu/comp152/SmallestInteger.java
@@ -1,5 +1,13 @@
 package bsu.comp152;
 
+/**
+ * SmallestInteger -
+ * A class for Project 2, COMP 152
+ *
+ * Starter code Laura K. Gross, laura.gross@bridgew.edu, 2/2022
+ *
+ * Completed by: [student name], [student email], [date of completion]
+ */
 public class SmallestInteger {
     public static void main(String[] args) {
 
